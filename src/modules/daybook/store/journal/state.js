c6bdd08567f3bfab561ec90800af45ce237734dc
ManriquezJM <manriquez.josematias@gmail.com>
2021-11-23 18:10:06 -0300

    primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,28 @@
+
+export default () => ({
+
+    isLoading:true,
+    entries:[
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text:'Aliqua ea amet nisi ad elit nulla mollit cupidatat esse enim quis est. Adipisicing est commodo officia mollit nulla elit.',
+            picture:null, 
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text:'Occaecat consectetur Lorem occaecat consectetur commodo esse nostrud ea sint.Occaecat consectetur Lorem occaecat consectetur commodo esse nostrud ea sint.',
+            picture:null, 
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text:'Occaecat consectetur Lorem occaecat consectetur commodo esse nostrud ea sint.Occaecat consectetur Lorem occaecat consectetur commodo esse nostrud ea sint.',
+            picture:null, 
+        },
+    ]
+    
+
+})
+
